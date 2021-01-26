fac96bc667a885289914bbddb546d4d8cce929c3
Egor754 <xel-boi@mail.ru>
2021-01-27 02:53:10 +0500

    two commit

--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseServerError
 from random import randint
 
 from django.db.models import Max, Min
@@ -12,8 +13,7 @@ def MainView(request):
         random = randint(0, len(tours) - 1)
         if tours[random] not in random_tours:
             random_tours.append(tours[random])
-    return render(request, 'tours/index.html',
-                  {'random_tours': random_tours})
+    return render(request, 'tours/index.html', {'random_tours': random_tours})
 
 
 def DepartureView(request, departure):
@@ -28,3 +28,12 @@ def TourView(request, id):
     tour = Tours.objects.select_related("departure").get(id=id)
     departure_ru = tour.departure.ru_departure
     return render(request, 'tours/tour.html', {'tour': tour, 'departure_ru': departure_ru})
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def ServerError(request):
+    return HttpResponseServerError('<h1>Извините, произошел '
+                                   'внутрисистемный сбой</h1>')
