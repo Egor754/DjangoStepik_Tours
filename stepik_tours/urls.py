72929540461d0dc2ed0f5bc625bedb082046240b
Egor754 <xel-boi@mail.ru>
2021-01-27 02:58:54 +0500

    two commit

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from tours.views import pageNotFound, ServerError
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tours.urls'))
 ]
+
+handler404 = pageNotFound
+handler500 = ServerError
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
--- tours/urls.py
+++ tours/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 
-from .views import MainView, DepartureView, \
-    TourView, pageNotFound, ServerError
+from .views import MainView, DepartureView,TourView
 
 urlpatterns = [
     path('', MainView, name='main'),
     path('departure/<str:departure>/', DepartureView, name='departure'),
     path('tour/<int:id>/', TourView, name='tour')
 ]
-
-handler404 = pageNotFound
-handler500 = ServerError
